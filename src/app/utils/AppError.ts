dd21b06cb75caa967d6c97e392228145b79c9b10
Ferdous Ahmed <himibaba10@gmail.com>
2025-07-10 22:13:10 +0600

    Created the custom AppError class

---
+++ src/app/utils/AppError.ts
@@ -0,0 +1,12 @@
+export class AppError extends Error {
+  constructor(message: string, public statusCode = 500, public stack = "") {
+    super(message);
+    this.statusCode = statusCode;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
