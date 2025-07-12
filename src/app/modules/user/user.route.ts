178f8e2aa6403b587d08c9046f6ae3c400ec2105
Ferdous Ahmed <himibaba10@gmail.com>
2025-07-12 23:47:18 +0600

    Created the zod schemas to check for createuser & upadate user validation

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import userControllers from "./user.controller";
+import userValidations from "./user.validation";
+import validateRequest from "../../utils/validateRequest";
 
 const userRoutes = Router();
 
 userRoutes.get("/", userControllers.getUsers);
 
-userRoutes.post("/register", userControllers.createUser);
+userRoutes.post(
+  "/register",
+  validateRequest(userValidations.createUserZodSchema),
+  userControllers.createUser
+);
 
 export default userRoutes;
